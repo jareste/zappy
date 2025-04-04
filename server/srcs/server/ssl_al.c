22e9da57bb1549908f496461d8c3d23726456204
jareste <jareste2000@gmail.com>
2025-04-04 16:55:56 +0200

    added rule for generating certs

--- server/srcs/server/ssl_al.c
+++ server/srcs/server/ssl_al.c
@@ -1,4 +1,3 @@
-// wss_server.c
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -18,13 +17,17 @@ static int m_sock_server = -1;
 
 void base64_encode(const unsigned char *input, int len, char *output)
 {
-    BIO *b64 = BIO_new(BIO_f_base64());
-    BIO *bio = BIO_new(BIO_s_mem());
+    BIO *b64;
+    BIO *bio;
+    BUF_MEM *buffer_ptr;
+
+    b64 = BIO_new(BIO_f_base64());
+    bio = BIO_new(BIO_s_mem());
     b64 = BIO_push(b64, bio);
-    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL); // No newlines
+    BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
     BIO_write(b64, input, len);
     BIO_flush(b64);
-    BUF_MEM *buffer_ptr;
+
     BIO_get_mem_ptr(b64, &buffer_ptr);
     memcpy(output, buffer_ptr->data, buffer_ptr->length);
     output[buffer_ptr->length] = 0;
@@ -34,24 +37,27 @@ void base64_encode(const unsigned char *input, int len, char *output)
 void websocket_handshake(SSL *ssl, const char *request)
 {
     char sec_websocket_key[256] = {0};
-    const char *key_header = strstr(request, "Sec-WebSocket-Key: ");
+    char response[512];
+    char key_guid[300];
+    char accept_key[128];
+    const char *key_header;
+
+    /* Extract Sec-WebSocket-Key from request */
+    key_header = strstr(request, "Sec-WebSocket-Key: ");
     if (!key_header) return;
     sscanf(key_header, "Sec-WebSocket-Key: %255s", sec_websocket_key);
 
-    // Combine key + magic GUID
-    char key_guid[300];
+    /* Combine key + magic GUID */
     snprintf(key_guid, sizeof(key_guid), "%s258EAFA5-E914-47DA-95CA-C5AB0DC85B11", sec_websocket_key);
 
-    // SHA1 hash
+    /* SHA1 hash */
     unsigned char sha1_hash[SHA_DIGEST_LENGTH];
     SHA1((unsigned char*)key_guid, strlen(key_guid), sha1_hash);
 
-    // Base64 encode
-    char accept_key[128];
+    /* Base64 encode */
     base64_encode(sha1_hash, SHA_DIGEST_LENGTH, accept_key);
 
-    // Send response
-    char response[512];
+    /* Send response */
     snprintf(response, sizeof(response),
         "HTTP/1.1 101 Switching Protocols\r\n"
         "Upgrade: websocket\r\n"
@@ -65,7 +71,8 @@ void send_ws_message(SSL *ssl, const char *msg)
 {
     size_t len = strlen(msg);
     unsigned char frame[1024] = {0};
-    frame[0] = 0x81; // FIN + text frame
+
+    frame[0] = 0x81; /* FIN + text frame */
     if (len < 126)
     {
         frame[1] = len;
@@ -76,13 +83,15 @@ void send_ws_message(SSL *ssl, const char *msg)
 
 static int init_server()
 {
+    struct sockaddr_in addr = {0};
+
     m_sock_server = socket(AF_INET, SOCK_STREAM, 0);
     if (m_sock_server == -1)
     {
         perror("socket");
         return ERROR;
     }
-    struct sockaddr_in addr = {0};
+
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
@@ -125,8 +134,8 @@ int init_ssl_al()
     method = TLS_server_method();
     m_ctx = SSL_CTX_new(method);
 
-    if (SSL_CTX_use_certificate_file(m_ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0 ||
-        SSL_CTX_use_PrivateKey_file(m_ctx, "key.pem", SSL_FILETYPE_PEM) <= 0)
+    if (SSL_CTX_use_certificate_file(m_ctx, "certs/cert.pem", SSL_FILETYPE_PEM) <= 0 ||
+        SSL_CTX_use_PrivateKey_file(m_ctx, "certs/key.pem", SSL_FILETYPE_PEM) <= 0)
     {
         ERR_print_errors_fp(stderr);
         return ERROR;
@@ -155,22 +164,27 @@ int cleanup_ssl_al()
 
 SSL *ssl_al_accept_client()
 {
+    SSL *ssl;
+    struct sockaddr_in client_addr;
+    socklen_t len;
+    int client;
+    char buf[4096] = {0};
+
     if (m_sock_server == -1)
     {
         fprintf(stderr, "Server socket not initialized\n");
         return NULL;
     }
-
-    struct sockaddr_in client_addr;
-    socklen_t len = sizeof(client_addr);
-    int client = accept(m_sock_server, (struct sockaddr*)&client_addr, &len);
+    
+    len = sizeof(client_addr);
+    client = accept(m_sock_server, (struct sockaddr*)&client_addr, &len);
     if (client == -1)
     {
         perror("accept");
         return NULL;
     }
 
-    SSL *ssl = SSL_new(m_ctx);
+    ssl = SSL_new(m_ctx);
     SSL_set_fd(ssl, client);
 
     if (SSL_accept(ssl) <= 0)
@@ -184,8 +198,6 @@ SSL *ssl_al_accept_client()
     printf("Client connected: %s:%d\n",
            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-
-    char buf[4096] = {0};
     SSL_read(ssl, buf, sizeof(buf) - 1);
     printf("Got handshake request:\n%s\n", buf);
 
@@ -199,36 +211,58 @@ SSL *ssl_al_accept_client()
 
 int socket_main()
 {
+    int bytes;
+    SSL *ssl;
+    unsigned char buffer[1024];
+    int payload_len;
+    unsigned char *mask;
+    unsigned char *data;
+
     init_ssl_al();
     while (1)
     {
-        SSL* ssl = ssl_al_accept_client();
+        ssl = ssl_al_accept_client();
         if (!ssl) {
             fprintf(stderr, "Failed to accept client\n");
             continue;
         }
 
-        unsigned char buffer[1024];
-        while (1) {
-            int bytes = SSL_read(ssl, buffer, sizeof(buffer));
-            if (bytes <= 0) break;
+        while (1)
+        {
+            bytes = SSL_read(ssl, buffer, sizeof(buffer));
+            if (bytes <= 0)
+            {
+                if (bytes <= 0)
+                {
+                    if (bytes == 0)
+                    {
+                        printf("Client disconnected\n");
+                    }
+                    else
+                    {
+                        fprintf(stderr, "SSL_read error\n");
+                        ERR_print_errors_fp(stderr);
+                    }
+                    break;
+                }
         
-            // Decode very basic frame (just text frame with small payload)
-            if ((buffer[0] & 0x0F) == 0x1) { // Text frame
-                int payload_len = buffer[1] & 0x7F;
-                unsigned char *mask = &buffer[2];
-                unsigned char *data = &buffer[6];
+            }
         
-                for (int i = 0; i < payload_len; i++) {
-                    data[i] ^= mask[i % 4]; // Unmask
+            if ((buffer[0] & 0x0F) == 0x1)
+            {
+                payload_len = buffer[1] & 0x7F;
+                mask = &buffer[2];
+                data = &buffer[6];
+        
+                for (int i = 0; i < payload_len; i++)
+                {
+                    data[i] ^= mask[i % 4]; /* Unmask */
                 }
         
                 printf("Client says: %.*s\n", payload_len, data);
                 send_ws_message(ssl, "Message received!");
             }
         }
-
-
 
         SSL_shutdown(ssl);
         SSL_free(ssl);
