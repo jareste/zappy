df08cbcbf5e3b51d02e3de9ab6ae605575d24fdb
jareste <jareste2000@gmail.com>
2025-04-21 11:36:19 +0200

    added randomizer for commands as AI is not working yet but i could test game

--- client/src/main/java/com/example/Command.java
+++ client/src/main/java/com/example/Command.java
@@ -1,17 +1,34 @@
 package com.example;
 
+import java.util.Random;
+
 public class Command {
     public String name;
     public String argument;
+    private static final Random random = new Random(); // Define the Random instance
 
     public Command(String name) {
         this.name = name;
         this.argument = "";
+        if (name.equals("prend") || name.equals("pose")) {
+            String[] possibleArgs = {"norriture", "linemate", "deraumere", "sibur", "mendiane", "phiras", "thystame"};
+            String randomCommand = possibleArgs[random.nextInt(possibleArgs.length)];
+            this.argument = randomCommand;
+            System.out.println("Random argument for " + name + ": " + this.argument);
+        }
     }
 
     public Command(String name, String argument) {
         this.name = name;
-        this.argument = argument;
+
+        if (name.equals("prend") || name.equals("pose")) {
+            String[] possibleArgs = {"norriture", "linemate", "deraumere", "sibur", "mendiane", "phiras", "thystame"};
+            String randomCommand = possibleArgs[random.nextInt(possibleArgs.length)];
+            this.argument = randomCommand;
+            System.out.println("Random argument for " + name + ": " + this.argument);
+        } else {
+            this.argument = argument;
+        }
     }
 
     public String getName() {
